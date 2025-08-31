44793791941d4bb309cf3d8de9c3dc6494e4d915
GitHub Action <action@github.com>
2025-08-31 16:01:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/prudhvir3ddy/jbinpb/services/MyProjectService.kt
+++ src/main/kotlin/com/github/prudhvir3ddy/jbinpb/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.prudhvir3ddy.jbinpb.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.prudhvir3ddy.jbinpb.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/prudhvir3ddy/jbinpb/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/prudhvir3ddy/jbinpb/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.prudhvir3ddy.jbinpb.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.prudhvir3ddy.jbinpb.MyBundle
+import com.github.prudhvir3ddy.jbinpb.services.MyProjectService
 import javax.swing.JButton
 
 
